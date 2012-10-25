798a2cea7ae426c4410d0e5f45c94ffc9d02840d
Cameron Morris <zephyrz2712@gmail.com>
2012-10-25 12:08:04 -0400

    Added eclipse project

---
+++ project/pong/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 25, 2012
+ *      Author: Cam
+ */
+
+#include <iostream>
+
+
+
+int main( int argc, char *argv[]) {
+
+
+	std::cout << "Hello world" << std::endl;
+
+	return 0;
+
+}
+
