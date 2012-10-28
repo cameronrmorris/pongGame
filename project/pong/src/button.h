6c200e23c052c503852245a3c615796abeedc548
Cameron Morris <zephyrz2712@gmail.com>
2012-10-27 20:10:07 -0400

    Started button class for menu

---
+++ project/pong/src/button.h
@@ -0,0 +1,30 @@
+/*
+ * button.h
+ *
+ *  Created on: Oct 27, 2012
+ *      Author: Cam
+ */
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+enum BUTTON_STATES {
+
+	MOUSE_OVER,
+	MOUSE_OUT,
+	MOUSE_DOWN,
+	MOUSE_UP
+
+};
+
+class button {
+
+public:
+
+private:
+
+
+};
+
+
+#endif /* BUTTON_H_ */
